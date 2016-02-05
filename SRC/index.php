47cfb6971af99d2b0f6551fbd9e9c2b9a0f02573
zappy1333 <zappythepro@gmail.com>
2016-02-05 18:36:13 +0000

    Added banner

--- SRC/index.php
+++ SRC/index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 
 <html>
+
 <head>
-<?php include 'head.html';?>
-<link rel="stylesheet" type="text/css" href="../Assets/custom.css">
+  <?php include 'head.html';?>
+  <link rel="stylesheet" type="text/css" href="../Assets/custom.css">
 </head>
 
 <body>
-<span id=banner>World Risk</span>
+  <div class="container-fluid">
+    <div id="banner"><h1 style= "text-align: center; padding: 10px">World Risk</h1></div>
+  </div>
 </body>
+
 </html>
