a5c157d26fb00e737f1360d8b2c8f6fb7bfe1ab5
zappy1333 <zappythepro@gmail.com>
2016-02-13 11:59:53 +0000

    Added canvas and made mobile compatible

--- SRC/index.php
+++ SRC/index.php
@@ -8,13 +8,41 @@
 </head>
 
 <body>
-  <div class="container-fluid">
-    <div id="banner">
-      <button type="button" class="btn btn-default btn-lg">
-        <span class="glyphicon glyphicon-nav" aria-hidden="true"></span> Setting
-      </button>
-      <h1 style= "text-align: center; padding: 10px">World Risk</h1>
+  <div class="container-full">
+    <div class="container-full" id="topbar">
+      <div id="banner">
+        <h1 style="text-align: center; padding: 10px">World Risk</h1>
+      </div>
+      <nav class="navbar-default" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+
+          <!-- Nav links -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li><a href="#">Stuff</a></li>
+              <li><a href="#">More stuff</a></li>
+              <li><a href="#">Hello</a></li>
+              <li><a href="#">Add stuff</a></li>
+              <li><a href="#">#WR2016</a></li>
+            </ul>
+          </div>
+        </div>
     </div>
+    <div id="warning-messge">
+      <h1>Please flip your device to landscape mode to view this page...</h1>
+    </div>
+    <canvas id="Map">
+    </canvas>
+    </nav>
+  </div>
   </div>
 </body>
 
